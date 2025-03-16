fbbce2bc697c638e5bf50e239de88cf965d98cf2
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-03-16 10:46:10 +0100

    Improved PDF conversion script

--- scripts/convert_pdf.py
+++ scripts/convert_pdf.py
@@ -1,10 +1,33 @@
-from pdf2image import convert_from_path, convert_from_bytes
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
+import multiprocessing, sys, os, time, tempfile, shutil
+from pdf2image import pdfinfo_from_path, convert_from_path
 
-images = convert_from_path("BS2024.pdf", thread_count=20)
-for i in range(len(images)):
-    images[i].save("page" + str(i) + ".jpg", "JPEG")
+if (len(sys.argv) == 1):
+    print("Missing target PDF path")
+    sys.exit()
+
+pdf_path = sys.argv[1]
+pdf_info = pdfinfo_from_path(sys.argv[1])
+pages_count = pdf_info["Pages"]
+threads = multiprocessing.cpu_count()
+
+temp_folder = tempfile.mkdtemp() # Using a temporary directory yields better performance when using multiple threads
+output_folder = "pages"
+
+print("Beginning conversion of {pages} pages from {path} with {count} threads".format(pages=pages_count, path=pdf_path, count=threads))
+
+start_time = time.time()
+
+os.makedirs(output_folder, exist_ok=True)
+
+images = convert_from_path(pdf_path, thread_count=threads, output_folder=temp_folder)
+
+for i, image in enumerate(images, start=1):
+    image_path = os.path.join(output_folder, "{}.png".format(i))
+    image.save(image_path, "PNG")
+
+end_time = time.time()
+
+shutil.rmtree(temp_folder)
+
+print("Job done in {:.3f}s".format(end_time - start_time))
+print("You can find the output at {}".format(os.path.abspath(output_folder)))
